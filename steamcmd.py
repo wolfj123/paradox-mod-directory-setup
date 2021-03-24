66820793616deac0f6a099374da48cee1d043f09
Unknown <wolfj@post.bgu.ac.il>
2021-03-24 22:26:42 +0200

    collect info from mod page

--- steamcmd.py
+++ steamcmd.py
@@ -2,32 +2,36 @@ import os
 import subprocess
 import time
 import shutil
+import urllib.request
+import re
 
 
 def main():
     steamcmd_path = "E:\steamcmd"
     steamcmd_exe = os.path.join(steamcmd_path, "steamcmd.exe")
     login_command = "login anonymous"
-    download_command = "workshop_download_item {game_id} {mod_id}" 
-    download_dir = os.path.join(steamcmd_path, "steamapps", "workshop", "content", "{game_id}", "{mod_id}") 
     url = ""
-    # game_id , mod_id = collect_info_from_url(url)
-    # download_command = download_command.format(game_id = game_id, mod_id = mod_id)
-    # download_dir = download_dir.format(game_id = game_id, mod_id = mod_id)
-    download_command = download_command.format(game_id = "281990", mod_id = "1610578060")
-    download_dir = download_dir.format(game_id = "281990", mod_id = "1610578060")
+
+    game_id , mod_id = collect_info_from_url("https://steamcommunity.com/sharedfiles/filedetails/?id=2428511640")
+    download_command = "workshop_download_item {game_id} {mod_id}".format(game_id = game_id, mod_id = mod_id)
+    download_dir = os.path.join(steamcmd_path, "steamapps", "workshop", "content", "{game_id}", "{mod_id}").format(game_id = game_id, mod_id = mod_id)
     download_mod(steamcmd_exe, login_command, download_command)
-    move_downloaded_dir_to_cwd(download_dir, "1610578060")
+    move_downloaded_dir_to_cwd(download_dir, mod_id)
 
 def collect_info_from_url(url):
-    print("TODO")
+    mod_id = re.findall('id=(\d+)', url)[0]
+    f = urllib.request.urlopen(url)
+    url_source = f.read().decode("utf-8")
+    game_id = re.findall('\t<a href="https://steamcommunity.com/app/(\d+)', url_source)[0]
+    return game_id, mod_id
 
 def download_mod(steamcmd_exe, login_command, download_command):
     os.system("{steamcmd_exe} +{login_command} +{download_command} +quit".format(steamcmd_exe = steamcmd_exe, login_command = login_command, download_command = download_command))
 
 def move_downloaded_dir_to_cwd(download_dir, mod_id):
-    print(os.getcwd())
-    shutil.move(download_dir, os.path.join(os.getcwd(), mod_id))
-
+    try:
+        shutil.move(download_dir, os.path.join(os.getcwd(), mod_id))
+    except:
+        print("FAILED")
 
 main()
