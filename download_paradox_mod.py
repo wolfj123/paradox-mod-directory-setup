4c5a83410b191d4fdcdd36557d2f52dd13a7cd58
Unknown <wolfj@post.bgu.ac.il>
2021-04-01 13:14:48 +0300

    using proper chromedrive

--- download_paradox_mod.py
+++ download_paradox_mod.py
@@ -6,12 +6,14 @@ import urllib.request
 import re
 import sys
 
-from selenium import webdriver  
+from selenium import webdriver
 
+chromedriver_path = r"C:\Users\user\Documents\My Documents\Tools\chromedriver\chromedriver_89.exe"
+# chromedriver_path = "C:\\Users\\user\\Documents\\My Documents\\Tools\\chromedriver\\chromedriver.exe"
+steamcmd_path = "E:\steamcmd"
 
 args = sys.argv.copy()
 args.pop(0)
-
 
 def main():
     for url in args:
@@ -21,13 +23,25 @@ def main():
         # move_downloaded_dir_to_cwd()
 
 def get_mod_download_url(mod_url):
-    driver = webdriver.Chrome()
     zip_url = ''
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver = webdriver.Chrome(chromedriver_path, options = options)  # Optional argument, if not specified will search path.
     driver.get(mod_url)
-    # WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.ID,"ptifrmtgtframe")))
-    # WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//span[@id='HRS_APPL_WRK_HRS_PAGE_TITLE']")))
-    time.sleep(4)
-    print (driver.page_source.encode('utf-8')) 
+    time.sleep(5) # Let the user actually see something!
+    url_source = driver.page_source
+
+    print("********************* INFO *********************************")
+    # info = re.findall('url\("https://modscontent.paradox-interactive.com/([\d|[a-zA-Z]]+)/([\d|[a-zA-Z]|\-]+)', url_source)
+    info = re.findall('https://modscontent.paradox-interactive.com/([\d|[a-zA-Z]]+)/', url_source)
+    print(info)
+
+    # game_code = info[0]
+    # mod_code = info[1]
+    # mod_latest_version = re.findall('src-components-Mods-Details-Header-styles__value--2VrqZ">(\d+)<', url_source)[0]
+ 
+    driver.quit()
+
     
     # f = urllib.request.urlopen(mod_url)
     # url_source = f.read().decode("utf-8")
@@ -47,5 +61,16 @@ def download_mod(zip_url):
 
 def move_downloaded_dir_to_cwd(download_dir, mod_id):
     print("TODO")
+
+
+def chromedirver_example():
+    driver = webdriver.Chrome(chromedriver_path)  # Optional argument, if not specified will search path.
+    driver.get('http://www.google.com/')
+    time.sleep(5) # Let the user actually see something!
+    search_box = driver.find_element_by_name('q')
+    search_box.send_keys('ChromeDriver')
+    search_box.submit()
+    time.sleep(5) # Let the user actually see something!
+    driver.quit()
 
 main()
